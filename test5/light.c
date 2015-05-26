02d8a8235455326f45bd2151a67913832d832327
Abhinav <abhinav@ubuntu.(none)>
2015-05-26 23:59:49 +0530

    hj

---
+++ test5/light.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+int main()
+{
+	int duration[]={5,2,3};
+	int i,j, k;
+	int poles[4]={0x09,0x08,0x01,0x04};
+	int tick=0;
+	while (1)
+	{
+		tick++;
+		for (i = 0; i < sizeof(poles) / sizeof(int); i++)
+		{
+			if ((poles[i] & 0x06) !=0) {
+				j=i;
+				break;
+			}
+		}
+		if (poles[j]==0x02)
+		{
+			if (tick>=duration[1])
+			{
+				poles[j]=0x04;
+				tick=0;
+			    poles[(j+1)%4]=0x09;
+			    poles[(j+2)%4]=0x08;
+			}
+		}
+		else if (poles[j]==0x04)
+		{
+			if (tick>=duration[2])
+			{
+				poles[j]=0x01;
+				tick=0;
+			    poles[(j+1)%4]=0x02;
+			    poles[(j+2)%4]=0x01;
+			}
+		}
+		for( k=0; k < sizeof(poles)/sizeof(int); k++){
+			printf("%0x ",poles[k]);			
+		}
+		printf("\n");
+		sleep(1);
+	}
+}
