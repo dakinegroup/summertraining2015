633dc5902ca9f062b6f53523eac3df8099a0f9da
Abhinav Ghai <abhiitmu@gmail.com>
2015-06-17 17:27:16 +0530

    counter'll b completed by tomorrow

--- test5/h.c
+++ test5/h.c
@@ -4,28 +4,28 @@ char * getBinaryString(unsigned int);
 void runTrafficLight(unsigned int);
 int main()
 {
-	unsigned int a=0x9114;
-	runTrafficLight(a);
+  unsigned int a=0x9114;
+  runTrafficLight(a);
 }
 
 char binaryString[50];
 char * getBinaryString(unsigned int bytes) {
-	int i = 0;
-	for(;i<16; i++) {
-		binaryString[i*2] =  ((bytes << i)&0x8000)?'1':'0';
-		binaryString[i*2+1] = ' ';
-	}
-	binaryString[i*2] = 0;
-	return &binaryString[0];
+  int i = 0;
+  for(;i<16; i++) {
+    binaryString[i*2] =  ((bytes << i)&0x8000)?'1':'0';
+    binaryString[i*2+1] = ' ';
+  }
+  binaryString[i*2] = 0;
+  return &binaryString[0];
 }
 
 void shiftOutClockedData(data) {
-	printf("%s\n", getBinaryString(data));
+  printf("%s\n", getBinaryString(data));
 }
 void runTrafficLight(unsigned int startingState) {
-	int i=0,counter=0,ctr1,ctr2,st1,st2,b,c;
-	unsigned int tState;
-	shiftOutClockedData(startingState);   
+  int i=0,counter=0,ctr1,ctr2,st,glt,b,c;
+  unsigned int tState;
+  shiftOutClockedData(startingState);   
   int tick = 0;                  
   int duration[]= {5,4,8};                         
 do {
@@ -33,12 +33,12 @@ do {
 
    for(i=0; i < 4; i ++) { 
      tState  = (startingState >> (i*4)) & 0x0F;
-      if(tState & 0x01) {/* Red is on */
+     if(tState & 0x01) {
        
-     }
-     else if(tState & 0x02) 
-                {
-                if (tick>=duration[1])
+     } else if(tState & 0x02) 
+                 {
+                 
+              if (tick>=duration[1])
                 {
                  startingState = (startingState & ~(0x0F << i*4)) | (0x04 << i*4) ;
                  tick=0;
@@ -52,35 +52,36 @@ do {
                 }  
                 shiftOutClockedData(startingState);
         
+        
       }
      } else if(tState & 0x04) {
-              printf("enter sleep time\n");
-                  scanf("%d",&st1); 
+      printf("enter sleep time\n");
+                  scanf("%d",&st); 
                   do
                   {
-                    sleep(st1);
+                    sleep(st);
                     counter++;
                     ctr1=counter;
                     tick++;
-                  }while(counter<((duration[2])/st1));
+                  }while(counter<((duration[2])/st));
                   printf("%d\n",ctr1);
                   counter=0;
                    printf("enter sleep time2\n");
-                  scanf("%d",&st2); 
+                  scanf("%d",&st); 
                   do
                   {
-                    sleep(st2);
+                    sleep(st);
                     counter++;
                     ctr2=counter;
                     
                     tick++;
-                  }while(counter<((duration[2])/st2));
+                  }while(counter<((duration[2])/st));
                   printf("%d\n",ctr2);
                   counter = 0;
-                              
-      if (tick>=duration[2])
+       
+       if (tick>=duration[2])
       {
-        if (ctr1 > ctr2)
+       if (ctr1 > ctr2)
         {
           b = 0x02;
           if (i==0 || i==1 || i==2)
@@ -109,26 +110,20 @@ do {
             startingState = ((c<<4) | ~(0xEEEE)) ^ (0x01<<4); 
           }
           
-        }
-        /*else if (ctr1 == ctr2)
-        {
-          startingState = (startingState & ~(0x0F << i*4)) | (0x01 << i*4) ;
-          if(i == 0){
-        startingState = (startingState & ~(0x0F << ((i+3)*4))) | (0x02 << ((i+3)*4)) ;
-       }
-        else {
-         startingState = (startingState & ~(0x0F << ((i-1)*4))) | (0x02 << ((i-1)*4)) ;
-       }
-        }*/
-        
-       
+        } 
   shiftOutClockedData(startingState);
   
       }
 
-      }
+      } else if(tState & 0x08) {
+        
+     }
    }
 
    sleep(1);  
   } while (1);
 }
+
+
+
+
