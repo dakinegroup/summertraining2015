edd7fb59eb28015ec1a90818405384b896d4788d
Vineet Maheshwari <vineet@dakinegroup.com>
2015-06-19 19:56:55 +0530

    updates..

--- test9/include/usart.h
+++ test9/include/usart.h
@@ -1,3 +1,20 @@
+#ifdef __USART_H__
+#define __USART_H__
+
+#define STATS_ENABLED 0
+#define BUFFER_SIZE  200
+#define RX_BUFFER_SIZE  100
+
 void USART_Init( unsigned int ubrr);
-//void USART_Transmit(unsigned char c);
-//void USART_Transmit_String(unsigned char *str);
+void printByte(unsigned char p);
+void USART_Transmit_String_P(const char *x);
+void USART_Transmit_Bytes_P();
+void USART_Transmit_String(char *x);
+void USART_Transmit_String2(char *x);
+#if STATS_ENABLED
+void USART_PrintStats()
+#endif
+unsigned char* USART_Receive_String();
+int USART_Receive_String2(char *buff, int len);
+
+#endif /* __USART_H__ */
--- test9/lib/usart.c
+++ test9/lib/usart.c
@@ -1,35 +1,96 @@
+/* File: usart.c
+ * Copyright (C) da Kine Technologies (P) Ltd. - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Vineet Maheshwari <vineet@dakinegroup.com>, June 2015
+ * Revision History
+ * Date        Ticket   Change Note
+ * -----------------------------------
+ * 20-Jun-2015  N/A     First Version Ready
+ * 
+ */
+
+/*
+File description:
+  Provides for USART Interrupt handling for transmitter and receiver
+  Following user interface functions are avialable:
+  a. USART_Init -- initialize interrupts, baud
+  b. USART_Transmit_String -- schedules a string for serial transmission
+  c. USART_Transmit -- serially transmits only one byte/char
+  d. USART_Receive -- checks if there is anything received from serial
+  e. USART_Transmit_String_P -- transmits strings from program memory
+  f. USART_Transmit_Bytes_P -- transmits data in bytes from program memory
+  g. USART_Receive_String -- receives complete line of tex
+  h. USART_Receive_String -- incomplete, but intends to retrieve machine to 
+     machine talk..
+*/
 #include <avr/io.h>
 #include <avr/pgmspace.h>
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
 #include <stdlib.h>
+#include "usart.h"
 
+/* Remember there is only 2k of RAM */
 
-#define BUFFER_SIZE  100
-#define EMPTY_THRESHOLD  10
-volatile unsigned char tx_buffer[100];
-volatile unsigned char tx_buffer_last_write = 0;
-volatile unsigned char tx_buffer_last_read = 0;
-volatile char tx_buffer_nearly_full = 0;
-volatile char tx_buffer_empty = 1;
+struct{
+	unsigned char buffer[BUFFER_SIZE];
+	unsigned char last_write;
+	unsigned char last_read;
+	char nearly_full;
+	char empty;
+} txQueue;
 
+struct {
+	unsigned char buffer[RX_BUFFER_SIZE];
+	unsigned char last_write;
+	unsigned char last_read;
+} rxQueue;
+
+#if STATS_ENABLED
+struct  {
+	int logs_truncated;
+	int logs_dropped;
+	int logs_delivered;
+	int logs_silence_events;
+} usartStats;
+#endif
+
+void initRxQueue() {
+	int i;
+	for(i=0; i < RX_BUFFER_SIZE; i++) {
+			rxQueue.buffer[i] = 0;
+		}	
+	rxQueue.last_write = rxQueue.last_read = 0;
+}
+
+void initTxQueue() {
+	int i;
+	txQueue.last_write=0;
+	txQueue.last_read=0;
+	txQueue.nearly_full=0;
+	txQueue.empty=1;
+	for(i=0; i < BUFFER_SIZE; i++) {
+		txQueue.buffer[i] = 0;
+	}	
+}
 void USART_Init( unsigned int ubrr)
 {
 int i;
-/*Set baud rate */
-UBRR0H = (unsigned char)(ubrr>>8);
-UBRR0L = (unsigned char)ubrr;
-/*Enable receiver and transmitter */
-UCSR0B = UCSR0B | (1<<TXEN0) | (1<<RXEN0) ;
-//| (1 << UDRIE0);
-// | (1 << RXCIE0);
-tx_buffer_last_write=0;
-tx_buffer_last_read=0;
-tx_buffer_nearly_full=0;
-tx_buffer_empty=1;
-for(i=0; i < BUFFER_SIZE; i++) {
-	tx_buffer[i] = 0;
-}
+	/*Set baud rate */
+	UBRR0H = (unsigned char)(ubrr>>8);
+	UBRR0L = (unsigned char)ubrr;
+	/*Enable receiver and transmitter */
+	UCSR0B = UCSR0B | (1<<TXEN0) | (1<<RXEN0) ;
+	UCSR0B = UCSR0B |  (1 << TXCIE0) ;
+	// enable receiver interrupt
+	//| (1 << RXCIE0) ;
+	// not sure how does it help, currently using only tx complete interrupt, it suffices
+	//|(1 << UDRIE0);
+
+	// Initialize tx and rx buffer
+    initTxQueue();
+	initRxQueue();
 }
 
 void USART_Transmit(unsigned char data) {
@@ -42,9 +103,14 @@ while ( !( UCSR0A & (1<<UDRE0)) )
 UDR0 = data;
 }
 
-
+/*
+  Developer Notes:
+  this is done in initial stages, just preserved here. Better way to do
+  is to use sprintf, itoa functions. One need to see, which one generates
+  compact code here.
+*/
 void printByte(unsigned char p) {
-	unsigned char c;
+unsigned char c;
 	c = (p >> 4) & 0x0f;
 	if(c < 10) {
 		USART_Transmit('0' + c);	
@@ -52,7 +118,6 @@ void printByte(unsigned char p) {
 	USART_Transmit(c+'A'-10);			
 		//USART_Transmit(getCharacter(c));		
 	}
-//	USART_Transmit(c < 10? ('0' + c) : getCharacter(c));
 	c = p  & 0x0f;
 	if(c < 10) {
 		USART_Transmit('0' + c);	
@@ -63,10 +128,16 @@ void printByte(unsigned char p) {
 }
 
 
-
+/*
+  Developer Notes:
+  This was useful to read the contents from program memory, specifically strings
+  It need to be enhanced such that constant strings are saved in program memory and
+  variable data is picked from RAM. This will help conserve RAM while using debug
+  messages. Also, we need to create constants file for message strings as well.
+*/
 void USART_Transmit_String_P(const char *x) {
-	int i = 0;
-	char c;
+int i = 0;
+char c;
 	for(i=0; i < 100; i++) {
 			//printByte(pgm_read_byte((char *)(0x0000 + i)));
 		c = pgm_read_byte(&x[i]);
@@ -78,9 +149,17 @@ void USART_Transmit_String_P(const char *x) {
 	USART_Transmit('\n');
 }
 
+/*
+  Developer Notes:
+  This is done to do the dump of program memory for debugging purposes.
+  Future enhancements: Get the argument as address and length of the bytes to be
+  displayed. While doing this, one need to keep in mind, the tx buffer. Because
+  if this information is dumped continously, it will be truncated. Therefore, we
+  need to provide a user intervention asking for "more.."
+*/
 void USART_Transmit_Bytes_P() {
-	int i = 0;
-	char c;
+int i = 0;
+char c;
 	return;
 	for(i=0; i < 100; i++) {
 			//printByte(pgm_read_byte((char *)(0x0000 + i)));
@@ -92,9 +171,14 @@ void USART_Transmit_Bytes_P() {
 	USART_Transmit('\n');
 }
 
+/* Developer Notes:
+   This is a blocking call. It waits continously for transmission end and transfers next character
+   This is very slow and may be useful, where there is no backend work being done as part of main loop.
+   Interrupt based work is anyway happening.
+*/
 void USART_Transmit_String(char *x) {
-	int i = 0;
-	char c;
+int i = 0;
+char c;
 	for(i=0; i < 100; i++) {
 		c = x[i];
 		if(c)
@@ -104,168 +188,114 @@ void USART_Transmit_String(char *x) {
 	USART_Transmit('\r');
 	USART_Transmit('\n');
 }
-void testFunction2(){
-	int diff = 0;
-	USART_Transmit_String("Value of..");
-	printByte((tx_buffer_last_read & 0xff00) >> 8);
-	printByte(tx_buffer_last_read & 0x00ff);
-	printByte((tx_buffer_last_write & 0xff00) >> 8);
-	printByte(tx_buffer_last_write & 0x00ff);
 
-    if(tx_buffer_last_read != tx_buffer_last_write) {
-      
-      tx_buffer_empty = 0;
-      USART_Transmit_String("Transmitting");
-      //UDR0 = tx_buffer[tx_buffer_last_read];
-      tx_buffer_last_read++;
-      if(tx_buffer_last_read == BUFFER_SIZE) {
-        tx_buffer_last_read = 0;
-      }
-
-      if(tx_buffer_last_read == tx_buffer_last_write) {
-        tx_buffer_empty = 1;
-      } else {
-	       /* find difference between read and write pointers*/
-	      if(tx_buffer_last_read < tx_buffer_last_write) {
-	        diff = tx_buffer_last_write - tx_buffer_last_read;
-	      } else {
-	        diff = tx_buffer_last_write + BUFFER_SIZE - tx_buffer_last_read;
-	      }
-
-	      if((BUFFER_SIZE - diff) < EMPTY_THRESHOLD) {
-	        tx_buffer_nearly_full = 1;
-	      } else {
-	        tx_buffer_nearly_full = 0;
-	      }
-      }
-     } 	
-}
-
+/* Developer Notes:
+   PING PONG Buffer being maintained.
+   Further it can be enhanced to implement circular buffer with prevention for overwriting.
+*/
 ISR(USART_TX_vect)
 {
-	int diff = 0;
-//	return;
   cli();
-  if(tx_buffer_last_read < tx_buffer_last_write) {
-  UDR0 = tx_buffer[tx_buffer_last_read];
-  tx_buffer_last_read=tx_buffer_last_read+1;  	
+  if(txQueue.last_read < txQueue.last_write) {
+	  UDR0 = txQueue.buffer[txQueue.last_read];
+	  txQueue.last_read=txQueue.last_read+1;  	
   } else {
-  	tx_buffer_last_read = 0;
-  	tx_buffer_last_write= 0;
-  }  /* if(tx_buffer_last_read != tx_buffer_last_write) {
-      
-      tx_buffer_empty = 0;
-      UDR0 = tx_buffer[tx_buffer_last_read];
-      tx_buffer_last_read++;
-      if(tx_buffer_last_read == BUFFER_SIZE) {
-        tx_buffer_last_read = 0;
-      }
-
-      if(tx_buffer_last_read == tx_buffer_last_write) {
-        tx_buffer_empty = 1;
-      } else {
-	       // find difference between read and write pointers
-	      if(tx_buffer_last_read < tx_buffer_last_write) {
-	        diff = tx_buffer_last_write - tx_buffer_last_read;
-	      } else {
-	        diff = tx_buffer_last_write + BUFFER_SIZE - tx_buffer_last_read;
-	      }
-
-	      if((BUFFER_SIZE - diff) < EMPTY_THRESHOLD) {
-	        tx_buffer_nearly_full = 1;
-	      } else {
-	        tx_buffer_nearly_full = 0;
-	      }
-      }
-     } */           
+  	txQueue.last_read = 0;
+  	txQueue.last_write= 0;
+  }            
   sei();
 }
-//ISR(USART_TX_vect, ISR_ALIASOF(USART_UDRE_vect));
+//ISR(USART_UDRE_vect, ISR_ALIASOF(USART_TX_vect));
+/* Developer Notes:
+   Though it is written here, not much use is envisaged currently, till we have machine to
+   machine communications and a independent interrupt on receipt of a frame. Need to look
+   at such software interrupt.
+*/
+ISR(USART_RX_vect)
+{
+  char tchar;
+  cli();
+  if(rxQueue.last_write >= RX_BUFFER_SIZE) {
+     tchar = UDR0; //drop this byte;
+     UDR0 = tchar;
+  } else {
+  	rxQueue.buffer[rxQueue.last_write] = UDR0;
+     UDR0 = rxQueue.buffer[rxQueue.last_write];
+  	rxQueue.last_write++;
+  }
+  sei();
+}
 
-int logs_dropped = 0;
-int logs_truncated = 0;
+/* Developer Notes:
+   This is non-blocking call. It schedules data transfer - irrespective of string or raw data
+   In case transmission has gone silent, it detects and wakes up the transceiver for rest of
+   the transmission to happen using ISR
+*/
 void USART_Transmit_String2(char *x) {
-	int diff,i=0;
+	int diff,i=0, trunc = 0;
  	cli();
- 	if(tx_buffer_last_write == 0) {
-		for(i=0; i < 50 && x[i] != 0; i++) {
-			tx_buffer[i] = x[i];
-		}
-		tx_buffer_last_write = i;
-	}
-	sei();
-	return;
-	if(tx_buffer_nearly_full == 1)  {
-		logs_dropped++;
-	} else {
-/*		USART_Transmit_String("condition check");
-*/		while((x[i] != 0) && (i < 50)) {
-			tx_buffer[tx_buffer_last_write] = x[i];
-			tx_buffer_last_write++;
-			if(tx_buffer_last_write == BUFFER_SIZE) {
-				tx_buffer_last_write = 0;
-			}
-			i++;
-/*			USART_Transmit_String("value of x..");
-     USART_Transmit_String(x);
-			USART_Transmit_String("value of txb..");
-     USART_Transmit_String(tx_buffer);
-*/			if(tx_buffer_last_write == tx_buffer_last_read) {
-				logs_truncated ++;
+ 	if(txQueue.last_write == 0) {
+ 		UDR0 = '$';
+ 		#if STATS_ENABLED
+ 		usartStats.logs_silence_events++;
+ 		#endif
+		for(i=0; i < BUFFER_SIZE/2 && x[i] != 0; i++) {
+			txQueue.buffer[i] = x[i];
+			if((txQueue.last_write + i) >= (BUFFER_SIZE-1)) {
+				trunc = 1;
 				break;
 			}
 		}
-		//tx_buffer_last_write = 5;
-/*		printByte((tx_buffer_last_read & 0xFF00) >> 8);
-		printByte(tx_buffer_last_read & 0x00FF);
-*/
-      if(tx_buffer_last_read < tx_buffer_last_write) {
-        diff = tx_buffer_last_write - tx_buffer_last_read;
-      } else {
-        diff = tx_buffer_last_write + BUFFER_SIZE - tx_buffer_last_read;
-      }
-		if((BUFFER_SIZE - diff) < EMPTY_THRESHOLD) {
-        	tx_buffer_nearly_full = 1;
-      	} else {
-        	tx_buffer_nearly_full = 0;
-      	}
-/*		printByte((tx_buffer_empty & 0xFF00) >> 8);
-		printByte(tx_buffer_empty & 0x00FF);
-*/		if(tx_buffer_empty) {
-			tx_buffer_empty = 0;
-			if( UCSR0A & (1<<UDRE0)) {	
-				UDR0 = tx_buffer[tx_buffer_last_read];
-/*      USART_Transmit_String("Transmitting1");
-      USART_Transmit_String(tx_buffer);
-      USART_Transmit_String((tx_buffer + tx_buffer_last_read));
-*/				tx_buffer_last_read++;
-				if(tx_buffer_last_read == BUFFER_SIZE) {
-					tx_buffer_last_read = 0;
-				}
+		txQueue.last_write = i;
+		#if STATS_ENABLED
+		if(trunc) {
+			usartStats.logs_truncated++;
+		} else {
+			usartStats.logs_delivered++;
+		}
+		#endif
+	} else if(txQueue.last_write > 0 
+			&& txQueue.last_write < BUFFER_SIZE) {
+		for(i=0; i < BUFFER_SIZE/2 && x[i] != 0; i++) {
+			txQueue.buffer[i+txQueue.last_write] = x[i];
+			if((txQueue.last_write + i) >= (BUFFER_SIZE-1)) {
+				trunc = 1;
+				break;
 			}
 		}
-   }
-   sei();
-}
-void USART_Transmit_Bytes() {
-	int i = 0;
-	char c;
-	return;
-	/*for(i=0; i < 100; i++) {
-			//printByte(pgm_read_byte((char *)(0x0000 + i)));
-		c = *((char *)(0x0060 + i));
-	    printByte(c);
-	    USART_Transmit(' ');
+		#if STATS_ENABLED
+		if(trunc) {
+			usartStats.logs_truncated++;
+		} else {
+			usartStats.logs_delivered++;
+		}
+		#endif
+		txQueue.last_write = i + txQueue.last_write;
+	} else {
+		#if STATS_ENABLED
+		usartStats.logs_dropped++;
+		#endif
 	}
-	USART_Transmit('\r');
-	USART_Transmit('\n');*/
+	sei();
 }
-
-unsigned char USART_Receive_Byte() {
-	loop_until_bit_is_set(UCSR0A, RXC0);
-	return UDR0;
+#if STATS_ENABLED
+void USART_PrintStats() {
+	static char logMsg [100];
+	sprintf(logMsg, "\r\nDelivered: %d\r\nTruncated: %d\r\nDropped: %d\r\nSilence Events: %d\r\n",
+				 usartStats.logs_delivered,
+				 usartStats.logs_truncated,
+				  usartStats.logs_dropped,
+				  usartStats.logs_silence_events);
+	USART_Transmit_String2(logMsg);
 }
+#endif
 
+
+/* Developer Notes:
+   This is blocking call, we receive data / command from user.
+   Further enhancements - we can make it non-blocking and take a
+    callback for processing commands.
+*/
 unsigned char* USART_Receive_String() {
 	static char inputs[50];
 	unsigned char i=0;;
@@ -292,4 +322,28 @@ unsigned char* USART_Receive_String() {
 	return inputs;
 }
 
-
+/* Developer Notes:
+   Not much utility is understood here for user interface purposes
+   However, if there is a machine to machine communication, where we
+   don't have requirement to display what is coming in, it may be useful
+   to create receive routine here and identify frames, which are saved 
+   in independent buffers 
+*/
+int USART_Receive_String2(char *buff, int len) {
+	int i = 0;
+	cli();
+  if(rxQueue.last_read < rxQueue.last_write) {
+  	//there is data to be read
+  	for(i = 0; 
+  		(i < len-1) && ((i+rxQueue.last_read) < RX_BUFFER_SIZE-1); 
+  		i++) {
+  		buff[i] = rxQueue.buffer[rxQueue.last_read+i];
+  	}
+  	buff[i] = 0;
+  	rxQueue.last_read = i + rxQueue.last_read;
+  } else {
+  	// either it is same, in which case both the pointers should be reduced to zero
+	rxQueue.last_read = rxQueue.last_write = 0;  	
+  }
+  sei();
+}
